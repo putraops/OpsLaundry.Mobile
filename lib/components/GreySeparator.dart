ac7dbf58817562ddf0534d3c9c90ca3593cff26b
Putra Ompusunggu <putraops@gmail.com>
2023-02-22 13:07:55 +0700

    New Component
    - Add new SwitchGridList component
    - Add new GreySeparator

---
+++ lib/components/GreySeparator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_apps/constants/color.dart' as color;
+
+class GreySeparator extends StatelessWidget {
+  final double? height;
+
+  const GreySeparator({
+    super.key,
+    this.height = 10,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: height, color: color.backgroundColor,);
+  }
+}
